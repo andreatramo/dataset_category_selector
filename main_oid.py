26cd9a578f21a59e4c187ea0a1b5cd84227e4347
Andrea Gaetano Tramontano <andreagtramo@gmail.com>
2019-01-18 11:49:35 +0000

    Possibility to choose the dataset subdivision
    Now you can choose to divide the images in 3 groups (train, test and validation set), in 2 or only 1.

--- main_oid.py
+++ main_oid.py
@@ -1,6 +1,6 @@
 from openimage_db import OpenimageDB
 import tensorflow as tf
-from tfrecord_utils import TFRecordWriter
+from tfrecord_utils import write_tfrecord
 import os
 import random
 
@@ -8,19 +8,12 @@ import random
 tf.flags.DEFINE_string('input_label_map', "/home/andreatramo/datasets/openimage/my_oid_label_map.pbtxt",
                        'Path to the label map proto')
 
-tf.flags.DEFINE_string('input_train_box', "/home/andreatramo/datasets/openimage/openimage_original/annotations/train-annotations-bbox.csv",
+tf.flags.DEFINE_string('input_train_label', "/home/andreatramo/datasets/openimage/openimage_original/annotations/train-annotations-bbox.csv",
                        'Path to CSV containing train image bounding box annotations')
-tf.flags.DEFINE_string('input_val_box', "/home/andreatramo/datasets/openimage/openimage_original/annotations/validation-annotations-bbox.csv",
+tf.flags.DEFINE_string('input_val_label', "/home/andreatramo/datasets/openimage/openimage_original/annotations/validation-annotations-bbox.csv",
                        'Path to CSV containing validation image bounding box annotations')
-tf.flags.DEFINE_string('input_test_box', "/home/andreatramo/datasets/openimage/openimage_original/annotations/test-annotations-bbox.csv",
+tf.flags.DEFINE_string('input_test_label', "/home/andreatramo/datasets/openimage/openimage_original/annotations/test-annotations-bbox.csv",
                        'Path to CSV containing test image bounding box annotations')
-
-tf.flags.DEFINE_string('input_train_verification', "/home/andreatramo/datasets/openimage/openimage_original/annotations/train-annotations-human-imagelabels-boxable.csv",
-                       'Path to CSV containing train image-level labels annotations')
-tf.flags.DEFINE_string('input_val_verification', "/home/andreatramo/datasets/openimage/openimage_original/annotations/validation-annotations-human-imagelabels-boxable.csv",
-                       'Path to CSV containing validation image-level labels annotations')
-tf.flags.DEFINE_string('input_test_verification', "/home/andreatramo/datasets/openimage/openimage_original/annotations/test-annotations-human-imagelabels-boxable.csv",
-                       'Path to CSV containing test image-level labels annotations')
 
 tf.flags.DEFINE_string('input_train_dir', "/home/andreatramo/datasets/openimage/openimage_original/images/train",
                        'Path to the train image directory')
@@ -32,6 +25,8 @@ tf.flags.DEFINE_string('input_test_dir', "/home/andreatramo/datasets/openimage/o
 tf.flags.DEFINE_string('output_dir', "/home/andreatramo/datasets/openimage/my_openimage",
                        'Path to the output directory')
 
+tf.flags.DEFINE_string('subdivision_mode', 111,
+                       'Instruction about how to divide the data')
 FLAGS = tf.flags.FLAGS
 
 
@@ -40,32 +35,45 @@ def main():
     # check if there are all the input
     required_flags = [
         'input_label_map',
-        'input_train_box', 'input_val_box', 'input_test_box',
-        'input_train_verification', 'input_val_verification', 'input_test_verification',
-        'input_train_dir', 'input_val_dir', 'input_test_dir',
         'output_dir'
     ]
     for flag_name in required_flags:
         if not getattr(FLAGS, flag_name):
             raise ValueError('Flag --{} is required'.format(flag_name))
 
-    print("\n********** OpenImage **********")
+    step = 0
+    input_file = dict()
 
-    input_file = {0: [FLAGS.input_train_box, FLAGS.input_train_verification, FLAGS.input_train_dir, "train"],
-                  1: [FLAGS.input_val_box, FLAGS.input_val_verification, FLAGS.input_val_dir, "validation"],
-                  2: [FLAGS.input_test_box, FLAGS.input_test_verification, FLAGS.input_test_dir, "test"]}
+    if FLAGS.input_train_label is not None and FLAGS.input_train_dir is not None:
+        input_file[step] = [FLAGS.input_train_label, FLAGS.input_train_dir, "train"]
+        step += 1
+    elif FLAGS.input_train_label is None ^ FLAGS.input_train_dir is None:
+        raise ValueError('One between the train annotations path or the train images directory, is not present.')
+    if FLAGS.input_val_label is not None and FLAGS.input_val_dir is not None:
+        input_file[step] = [FLAGS.input_val_label, FLAGS.input_val_dir, "validation"]
+        step += 1
+    elif FLAGS.input_val_label is None ^ FLAGS.input_val_dir is None:
+        raise ValueError('One between the validation annotations path or the validation images directory, is not present.')
+    if FLAGS.input_test_label is not None and FLAGS.input_test_dir is not None:
+        input_file[step] = [FLAGS.input_test_label, FLAGS.input_test_dir, "test"]
+        step += 1
+    elif FLAGS.input_test_label is None ^ FLAGS.input_test_dir is None:
+        raise ValueError('One between the test annotations path or the test images directory, is not present.')
+
+    if step < 1:
+        raise ValueError('No train, test or validation path present. Give in input almost one of them.')
+
+    print("\n********** OpenImage **********")
 
     images_not_found = 0
 
-    step = 3
     img_list = []
 
     for i in range(step):
         now_input = [FLAGS.input_label_map,
                      input_file[i][0],
                      input_file[i][1],
-                     input_file[i][2],
-                     input_file[i][3]]
+                     input_file[i][2]]
         database = OpenimageDB(now_input, FLAGS.output_dir)
         img_list += database.img_selector()
         if i == 0:
@@ -74,46 +82,70 @@ def main():
             object_found += database.get_num_object_found()
         images_not_found += database.get_img_not_found()
 
+    print("Number of object found: " + str(object_found))
+    print("Number of images not found: " + str(images_not_found))
+
     # shuffle the entire dataset and divide it in train, test and validation set
 
     # shuffle list
     random.shuffle(img_list)
 
-    train_percentage = 0.8
-    val_percentage = 0.1
-    test_percentage = 1 - train_percentage - val_percentage
+    test_percentage = 0
+    val_percentage = 0
+    step = 0
+
+    if FLAGS.subdivision_mode == 111:
+        test_percentage = 0.1
+        val_percentage = 0.1
+        step = 3
+    elif FLAGS.subdivision_mode == 110:
+        val_percentage = 0.1
+        step = 2
+    elif FLAGS.subdivision_mode == 101:
+        test_percentage = 0.1
+        step = 2
+    elif FLAGS.subdivision_mode == 11:
+        test_percentage = 0.5
+        val_percentage = 0.5
+        step = 2
+    elif FLAGS.subdivision_mode == 0:
+        test_percentage = 0
+        val_percentage = 0
+        step = 1
+
+    train_percentage = 1 - test_percentage - val_percentage
 
     train_thr = int(len(img_list)*train_percentage)
-    val_thr = int(len(img_list)*val_percentage)
-    test_thr = int(len(img_list) * test_percentage)
+    val_thr = int(len(img_list)*val_percentage) + train_thr
+    test_thr = int(len(img_list) * test_percentage) + val_thr
 
     train_img = img_list[:train_thr]
     val_img = img_list[train_thr:val_thr]
-    test_img = img_list[val_thr:]
+    test_img = img_list[val_thr:test_thr]
 
     # write the images and labels in a TFRecord
-    for i in range(3):
-        if i == 0:
-            tfrecord_path = FLAGS.output_dir + "/" + "train_oid_dataset.tfrecord_1"
+    for i in range(step):
+        if step == 1:
+            tfrecord_path = FLAGS.output_dir + "/" + "my_oid_dataset_1"
             list_to_write = train_img
-        if i == 1:
-            tfrecord_path = FLAGS.output_dir + "/" + "val_oid_dataset.tfrecord_1"
-            list_to_write = val_img
-        if i == 2:
-            tfrecord_path = FLAGS.output_dir + "/" + "test_oid_dataset.tfrecord_1"
-            list_to_write = test_img
+        else:
+            if i == 0 and FLAGS.subdivision_mode > 100:
+                tfrecord_path = FLAGS.output_dir + "/" + "train_oid_dataset_1"
+                list_to_write = train_img
+            elif (i == 1 and FLAGS.subdivision_mode > 100) or (i == 0 and 0 < FLAGS.subdivision_mode < 100):
+                tfrecord_path = FLAGS.output_dir + "/" + "val_oid_dataset_1"
+                list_to_write = val_img
+            elif (i == 2 and FLAGS.subdivision_mode > 110) or (i == 1 and 0 < FLAGS.subdivision_mode < 100):
+                tfrecord_path = FLAGS.output_dir + "/" + "test_oid_dataset_1"
+                list_to_write = test_img
 
         n = 1
-        while os.path.exists(tfrecord_path):
+        while os.path.exists(tfrecord_path + ".tfrecord"):
             n += 1
             tfrecord_path = tfrecord_path[:len(tfrecord_path) - 1] + str(n)
 
-        writer = TFRecordWriter(tfrecord_path)
-        writer.write_tfrecord(list_to_write)
-        writer.close_tfrecord()
-
-    print("Number of object found: " + str(object_found))
-    print("Number of images not found: " + str(images_not_found))
+        tfrecord_path += ".tfrecord" 
+        write_tfrecord(tfrecord_path, list_to_write)
 
     return
 
